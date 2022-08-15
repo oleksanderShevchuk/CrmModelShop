89f70845f143653976680969a53a5c6cb3f9a3b7
oleksandr.shevchuk222@gmail.com <oleksandr.shevchuk222@gmail.com>
2022-08-15 14:16:09 +0300

    Исправлени ошибки добавления(клиента, продавца и продуктов)

--- CrmUi/CustomerForm.cs
+++ CrmUi/CustomerForm.cs
@@ -13,14 +13,14 @@ namespace CrmUi
         }
         public CustomerForm(Customer customer) : this()
         {
-            Customer = customer;
+            Customer = customer ?? new Customer();
             textBox1.Text = customer.Name;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var c = Customer ?? new Customer();
-            c.Name = textBox1.Text;
+            Customer = Customer ?? new Customer();
+            Customer.Name = textBox1.Text;
             Close();
         }
     }
--- CrmUi/SellerForm.cs
+++ CrmUi/SellerForm.cs
@@ -13,14 +13,14 @@ namespace CrmUi
         }
         public SellerForm(Seller seller) : this()
         {
-            Seller = seller;
+            Seller = seller ?? new Seller();
             textBox1.Text = seller.Name;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var s = Seller ?? new Seller();
-            s.Name = textBox1.Text;
+            Seller = Seller ?? new Seller();
+            Seller.Name = textBox1.Text;
             Close();
         }
     }
